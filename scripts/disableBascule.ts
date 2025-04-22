c4fe221ea96fdb16cd1d54d12ff9c2fa66cddbcb
Jules de Smit <julesdesmit@gmail.com>
2025-04-22 17:26:31 +0400

    Add disabling scripts

---
+++ scripts/disableBascule.ts
@@ -0,0 +1,54 @@
+import * as anchor from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { Lbtc } from "../target/types/lbtc";
+import { getBase58EncodedTxBytes, getConfigPDA } from "./utils";
+
+// Provide instructions.
+if (process.argv.indexOf("--help") > -1) {
+  console.log(`Usage: PROGRAM_ID=<program_id> ANCHOR_PROVIDER_URL=<rpc_url> ANCHOR_WALLET=<wallet_path> yarn disableBascule
+
+    Disables the bascule checks for minting instructions.`);
+  process.exit(0);
+}
+
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+
+// Check for program ID match.
+if (!process.env.PROGRAM_ID) {
+  console.error("no program Id set");
+  process.exit(1);
+}
+const programId = new PublicKey(process.env.PROGRAM_ID);
+const program = new anchor.Program(require("../target/idl/lbtc.json"), provider) as anchor.Program<Lbtc>;
+
+if (!program.programId.equals(programId)) {
+  console.error("the program id in the idl does not match the program id passed as env variable");
+  process.exit(1);
+}
+
+// If we have a populate flag at the end of the call, we return the bytes.
+let populate = process.argv.at(-1) === "--populate";
+
+(async () => {
+  try {
+    const payer = provider.wallet.publicKey; // Get wallet address
+
+    // Derive PDA for config
+    const configPDA = getConfigPDA(programId);
+    console.log("Using config PDA:", configPDA.toBase58());
+
+    const tx = await program.methods.disableBascule().accounts({
+      payer,
+      config: configPDA
+    });
+
+    if (populate) {
+      console.log("Transaction bytes:", await getBase58EncodedTxBytes(await tx.instruction(), provider.connection));
+    } else {
+      console.log("Transaction Signature:", await tx.rpc());
+    }
+  } catch (err) {
+    console.error("Error enabling bascule:", err);
+  }
+})();
