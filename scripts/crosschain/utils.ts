e57e5dbf1ea76bffcafb0ec07b036c0eb9703a50
Andrey B-L <andreyb@lombard.finance>
2026-04-05 01:23:12 +0400

    * added scripts to init TokenPool's chain config

--- scripts/crosschain/utils.ts
+++ scripts/crosschain/utils.ts
@@ -1,5 +1,6 @@
 import { PublicKey } from "@solana/web3.js";
-import { TOKEN_POOL_SIGNER_SEED, TOKEN_POOL_STATE_SEED } from "./constants";
+import * as anchor from "@coral-xyz/anchor";
+import { TOKEN_POOL_CHAIN_CONFIG_SEED, TOKEN_POOL_SIGNER_SEED, TOKEN_POOL_STATE_SEED } from "./constants";
 
 export function getTokenPoolSigner(mint: PublicKey, program: PublicKey) {
   return PublicKey.findProgramAddressSync([TOKEN_POOL_SIGNER_SEED, mint.toBytes()], program)[0];
@@ -7,4 +8,8 @@ export function getTokenPoolSigner(mint: PublicKey, program: PublicKey) {
 
 export function getTokenPoolState(mint: PublicKey, program: PublicKey) {
   return PublicKey.findProgramAddressSync([TOKEN_POOL_STATE_SEED, mint.toBytes()], program)[0];
+}
+
+export function getTokenPoolChainConfig(mint: PublicKey, foreignChainSelector: anchor.BN, program: PublicKey) {
+  return PublicKey.findProgramAddressSync([TOKEN_POOL_CHAIN_CONFIG_SEED, foreignChainSelector.toBuffer("le", 8), mint.toBytes()], program)[0];
 }
