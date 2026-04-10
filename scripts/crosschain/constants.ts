2fcef9a3953a59120f1dba3db9f992148fdaae64
andrey <andreyb@lombard.finance>
2026-04-10 13:14:09 +0300

    * more scripts to check different configs

--- scripts/crosschain/constants.ts
+++ scripts/crosschain/constants.ts
@@ -1,3 +1,4 @@
 export const TOKEN_POOL_SIGNER_SEED = Buffer.from("ccip_tokenpool_signer");
 export const TOKEN_POOL_STATE_SEED = Buffer.from("ccip_tokenpool_config");
 export const TOKEN_POOL_CHAIN_CONFIG_SEED = Buffer.from("ccip_tokenpool_chainconfig");
+export const BRIDGE_CONFIG_SEED = Buffer.from("bridge_config");
--- scripts/crosschain/utils.ts
+++ scripts/crosschain/utils.ts
@@ -1,6 +1,6 @@
 import { PublicKey } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import { TOKEN_POOL_CHAIN_CONFIG_SEED, TOKEN_POOL_SIGNER_SEED, TOKEN_POOL_STATE_SEED } from "./constants";
+import { BRIDGE_CONFIG_SEED, TOKEN_POOL_CHAIN_CONFIG_SEED, TOKEN_POOL_SIGNER_SEED, TOKEN_POOL_STATE_SEED } from "./constants";
 
 export function getTokenPoolSigner(mint: PublicKey, program: PublicKey) {
   return PublicKey.findProgramAddressSync([TOKEN_POOL_SIGNER_SEED, mint.toBytes()], program)[0];
@@ -12,4 +12,8 @@ export function getTokenPoolState(mint: PublicKey, program: PublicKey) {
 
 export function getTokenPoolChainConfig(mint: PublicKey, foreignChainSelector: anchor.BN, program: PublicKey) {
   return PublicKey.findProgramAddressSync([TOKEN_POOL_CHAIN_CONFIG_SEED, foreignChainSelector.toBuffer("le", 8), mint.toBytes()], program)[0];
+}
+
+export function getBridgeConfigPDA(program: PublicKey) {
+  return PublicKey.findProgramAddressSync([BRIDGE_CONFIG_SEED], program)[0];
 }
